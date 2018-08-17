cb0c00d5ebc3e2e6f78c4bf18715945000498b0f
Samer Zagzoog <mrsamer.dev@gmail.com>
2018-08-17 14:58:53 +0300

    Added api User Authintication

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://samer:samer00@ds243041.mlab.com:43041/devconnectordb'
+    mongoURI: 'mongodb://samer:samer00@ds243041.mlab.com:43041/devconnectordb',
+    secretOrKey: 'secret'
   };
   
